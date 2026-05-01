1d438631418c681d44772561f084d2c30b1526a9
chigozie <okoroaforc14@gmail.com>
2026-05-01 15:39:12 +0100

    chore: signin fix

--- src/controllers/auth/sign-in/index.ts
+++ src/controllers/auth/sign-in/index.ts
@@ -1,9 +1,11 @@
 import { Request, Response, NextFunction } from "express";
 import { InternalError } from "../../../helpers/errorHandler/errorHandler";
-import { validateDTO } from "../../../helpers/util";
+import { sendMail, validateDTO } from "../../../helpers/util";
 import { SignInDTO, fetchUserByEmail, validateUserPassword, getUserAccess, createJwtToken, getFirstLoginStatus } from "./util";
 import { success } from "../../../helpers/errorHandler/statusCodes";
 import { getSubscriptionForBusiness } from "../../subscriptions/find-subscription/util";
+import { generateVerificationLink, templateHTMl } from "../create-account/util";
+import util from "util";
 
 export async function SignInController(req: Request, res: Response, next: NextFunction) {
     try {
@@ -12,7 +14,19 @@ export async function SignInController(req: Request, res: Response, next: NextFu
 
         await validateUserPassword(dto.password, user.password);
 
-        // (user.verified)
+        if (!user.verified) {
+            const link = generateVerificationLink(user.uid)
+            const html = util.format(templateHTMl, dto.fullName, link, link)
+            // name,confirmationLink,confirmationLink
+            await sendMail(
+                {
+                    to: dto.email,
+                    subject: "New Account Verification",
+                    // html: `<p>Hey there,</p><p>Kindly verify your account using this link below: <b>${link}</b></p>`
+                    html
+                }
+            )
+        }
 
         const { accessType, permissions } = getUserAccess(user);
         let subscription
@@ -25,10 +39,10 @@ export async function SignInController(req: Request, res: Response, next: NextFu
             permissions,
             userProfileUid: user.userProfiles!.uid,
             businessUid: user?.userProfiles?.business?.uid
-        }); 
+        });
 
         const d = getFirstLoginStatus(user)
-        success(res, { token, accessType, proceedToProfileCreation: d.proceedToProfileCreation, isFirstLogin: d.isFirstLogin,isVerified: user.verified ,subscription }, "Sign in successful");
+        success(res, { token, accessType, proceedToProfileCreation: d.proceedToProfileCreation, isFirstLogin: d.isFirstLogin, isVerified: user.verified, subscription }, "Sign in successful");
     } catch (error) {
         next(new InternalError(error));
     }
